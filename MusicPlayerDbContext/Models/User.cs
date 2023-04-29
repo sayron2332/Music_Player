7ddd5e698ef050fa9b519e6d0b48e62e5d6f475f
sayron2332 <xvtnxjgbyv@gmail.com>
2023-04-29 17:07:26 +0300

    update-databasee

---
+++ MusicPlayerDbContext/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_acces.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public int Password { get; set; }
+        public IEnumerable<Playlist> Playlists { get; set; }
+    }
+}
